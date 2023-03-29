b39d78a26b47953a2f158b510888643f1cb27270
Deepika Salelkar <deepika.salelkar@gmail.com>
2023-03-28 22:11:52 -0700

    first commit

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num = 10,prod = 1;
+		for(int i =num; i>1;i--)
+		{
+			prod = prod * i;
+		}
+		System.out.println(prod);
+
+	}
+
+}
